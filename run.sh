cbd94b9bdd6ab9b1d922c1e321b1c7c7fa08d6a5
Bruno Almeida <brunoslalmeida@gmail.com>
2020-03-08 11:59:10 -0300

    adding restart

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 docker stop phpmyadmin
+docker rm phpmyadmin
 
-docker run -d --rm \
+docker run -d \
         --hostname phpmyadmin \
         --name phpmyadmin \
         --network internal \
+        --restart=always \
         -e PMA_ARBITRARY=1 \
         -e PMA_HOST=mysql \
-        phpmyadmin/phpmyadmin
+        phpmyadmin/phpmyadmin 
